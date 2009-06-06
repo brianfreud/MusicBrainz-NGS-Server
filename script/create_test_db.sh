brianfreud/MusicBrainz-NGS-Server

commit 10e3cacc72abba58575db7bcd3954966ef67b2b0

Migration script for the RAWDATA database
git-svn-id: http://svn.musicbrainz.org/mb_server/trunk@11671 b0b80210-5d09-0410-99dd-b4bd03f891c0

admin/sql/updates/ngs-rawdata.pl
@@ -1,0 +1,179 @@
+#!/usr/bin/perl -w
+
+use strict;
+use FindBin;
+use lib "$FindBin::Bin/../../../lib";
+
+use DBDefs;
+use MusicBrainz;
+use MusicBrainz::Server::Validation;
+use Sql;
+
+my $mb = MusicBrainz->new;
+$mb->Login(db => "READWRITE");
+my $sql = Sql->new($mb->dbh);
+
+my $raw_mb = MusicBrainz->new;
+$raw_mb->Login(db => "RAWDATA");
+my $raw_sql = Sql->new($raw_mb->dbh);
+
+$sql->Begin;
+$raw_sql->Begin;
+eval {
+
+print "Converting artist data\n";
+
+$raw_sql->Do("
+    INSERT INTO artist_rating_raw (artist, editor, rating)
+        SELECT artist, editor, rating FROM public.artist_rating_raw
+    ");
+
+$raw_sql->Do("
+    INSERT INTO artist_tag_raw (artist, editor, tag)
+        SELECT artist, moderator, tag FROM public.artist_tag_raw
+    ");
+
+print "Converting label data\n";
+
+$raw_sql->Do("
+    INSERT INTO label_rating_raw (label, editor, rating)
+        SELECT label, editor, rating FROM public.label_rating_raw
+    ");
+
+$raw_sql->Do("
+    INSERT INTO label_tag_raw (label, editor, tag)
+        SELECT label, moderator, tag FROM public.label_tag_raw
+    ");
+
+print "Converting recording data\n";
+
+$raw_sql->Do("
+    INSERT INTO recording_rating_raw (recording, editor, rating)
+        SELECT track, editor, rating FROM public.track_rating_raw
+    ");
+
+$raw_sql->Do("
+    INSERT INTO recording_tag_raw (recording, editor, tag)
+        SELECT track, moderator, tag FROM public.track_tag_raw
+    ");
+
+print "Converting release group data\n";
+
+
+print " * Loading release group map\n";
+my $albums = $sql->SelectListOfHashes("SELECT id, release_group FROM public.album");
+my %rg_map = map { $_->{id} => $_->{release_group} } @$albums;
+$albums = undef;
+
+$raw_sql->Do("CREATE AGGREGATE array_accum (basetype = anyelement, sfunc = array_append, stype = anyarray, initcond = '{}')");
+
+print " * Converting raw tags\n";
+my %aggr;
+$raw_sql->Select("
+    SELECT tag, moderator, array_accum(release)
+    FROM public.release_tag_raw
+    GROUP BY tag, moderator");
+while (1) {
+    my $row = $raw_sql->NextRowRef or last;
+    my ($tag, $editor, $ids) = @$row;
+    # Unpack the array if using old DBD::Pg
+    if (ref $ids ne 'ARRAY') {
+        $ids = [ $ids =~ /(\d+)/g ];
+    }
+    # Map album ids to release_group ids, remove duplicates
+    my %ids = map { $rg_map{$_} => 1 } @$ids;
+    my @ids = keys %ids;
+    foreach my $id (@ids) {
+        $raw_sql->Do("
+            INSERT INTO release_group_tag_raw (release_group, editor, tag)
+            VALUES (?, ?, ?)", $id, $editor, $tag);
+        unless (exists $aggr{$id}) {
+            $aggr{$id} = {};
+        }
+        if (exists $aggr{$id}{$tag}) {
+            $aggr{$id}->{$tag} += 1;
+        }
+        else {
+            $aggr{$id}->{$tag} = 1;
+        }
+    }
+}
+$raw_sql->Finish;
+
+print " * Converting aggregated tags\n";
+foreach my $id (keys %aggr) {
+    my %tags = %{ $aggr{$id} };
+    foreach my $tag (keys %tags) {
+        $sql->Do("
+            INSERT INTO release_group_tag (release_group, tag, count)
+            VALUES (?, ?, ?)", $id, $tag, $tags{$tag});
+    }
+}
+%aggr = ();
+
+print " * Converting raw ratings\n";
+# Iterate over all raw ratings
+$raw_sql->Select("
+    SELECT rating, editor, release
+    FROM public.release_rating_raw
+    ORDER BY editor");
+my $user_id = 0;
+my @user_data;
+while (1) {
+    my $row = $raw_sql->NextRowRef;
+    my $new_user_id = $row ? $row->[1] : 0;
+    if ($user_id != $new_user_id) {
+        # If this is a new user, process their ratings
+        if (@user_data) {
+            my %rg_rating_sum;
+            my %rg_rating_cnt;
+            # Map albums to RGs and calculate sums/counts
+            foreach $row (@user_data) {
+                my ($rating, $editor, $album) = @$row;
+                my $rg = $rg_map{$album};
+                $rg_rating_sum{$rg} += $rating;
+                $rg_rating_cnt{$rg} += 1;
+            }
+            # Iterate over unique RGs and add average raw ratings
+            foreach my $rg (keys %rg_rating_sum) {
+                my $rating = int(0.5 + $rg_rating_sum{$rg} / $rg_rating_cnt{$rg});
+                $raw_sql->Do("
+                    INSERT INTO release_group_rating_raw (release_group, editor, rating)
+                    VALUES (?, ?, ?)", $rg, $user_id, $rating);
+            }
+        }
+        $user_id = $new_user_id;
+        @user_data = ();
+    }
+    last if !$new_user_id;
+    push @user_data, [ @$row ];
+}
+
+print " * Converting average ratings\n";
+$sql->Do("CREATE UNIQUE INDEX tmp_release_group_meta_idx ON release_group_meta (id)");
+$raw_sql->Select("
+    SELECT release_group, avg(rating), count(*)
+    FROM release_group_rating_raw
+    GROUP BY release_group");
+while (1) {
+    my $row = $raw_sql->NextRowRef or last;
+    my ($id, $rating, $count) = @$row;
+    $sql->Do("UPDATE release_group_meta SET rating=?, ratingcount=? WHERE id=?", $rating, $count, $id);
+}
+$raw_sql->Finish;
+$sql->Do("DROP INDEX tmp_release_group_meta_idx");
+
+$raw_sql->Do("DROP AGGREGATE array_accum (anyelement)");
+
+print " * Converting CD stubs\n";
+$raw_sql->Do("INSERT INTO cdtoc_raw SELECT * FROM public.cdtoc_raw");
+$raw_sql->Do("INSERT INTO release_raw SELECT * FROM public.release_raw");
+$raw_sql->Do("INSERT INTO track_raw SELECT * FROM public.track_raw");
+
+    $sql->Commit;
+    $raw_sql->Commit;
+};
+if ($@) {
+    $sql->Rollback;
+    $raw_sql->Rollback;
+}

script/create_test_db.sh
@@ -4,7 +4,9 @@ set -o errexit
 cd `dirname $0`/..
 
 echo 'DROP SCHEMA musicbrainz_test CASCADE;' | ./admin/psql --profile=test READWRITE
+echo 'DROP SCHEMA musicbrainz_test CASCADE;' | ./admin/psql --profile=test RAWDATA
 echo 'CREATE SCHEMA musicbrainz_test;' | ./admin/psql --profile=test READWRITE
+echo 'CREATE SCHEMA musicbrainz_test;' | ./admin/psql --profile=test RAWDATA
 
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateTables.sql
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateFunctions.sql
@@ -16,5 +18,10 @@ echo 'CREATE SCHEMA musicbrainz_test;' | ./admin/psql --profile=test READWRITE
 
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateIndexes.sql
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateSearchIndexes.sql
+
+./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreateTables.sql
+./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreatePrimaryKeys.sql
+./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreateFKConstraints.sql
+./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreateIndexes.sql
 
 # eof

admin/GenerateSQLScripts.pl
@@ -5,143 +5,141 @@ use strict;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
-open FILE, "<$FindBin::Bin/../admin/sql/CreateTables.sql";
-my $create_tables_sql = do { local $/; <FILE> };
-close FILE;
+sub process_tables
+{
+    my ($dir) = @_;
 
-my @tables;
-my %foreign_keys;
-my %primary_keys;
-while ($create_tables_sql =~ m/CREATE TABLE\s+([a-z0-9_]+)\s+\(\s*(.*?)\s*\);/gs) {
-    my $name = $1;
-    my @lines = split /\n/, $2;
-    my @fks;
-    foreach my $line (@lines) {
-        if ($line =~ m/([a-z0-9_]+).*?\s*--.*?references ([a-z0-9_]+)\.([a-z0-9_]+)/) {
-            my @fk = ($1, $2, $3);
-            my $cascade = ($line =~ m/CASCADE/) ? 1 : 0;
-            push @fks, [@fk, $cascade];
+    open FILE, "<$FindBin::Bin/../admin/sql$dir/CreateTables.sql";
+    my $create_tables_sql = do { local $/; <FILE> };
+    close FILE;
+
+    my @tables;
+    my %foreign_keys;
+    my %primary_keys;
+    while ($create_tables_sql =~ m/CREATE TABLE\s+([a-z0-9_]+)\s+\(\s*(.*?)\s*\);/gs) {
+        my $name = $1;
+        my @lines = split /\n/, $2;
+        my @fks;
+        foreach my $line (@lines) {
+            if ($line =~ m/([a-z0-9_]+).*?\s*--.*?references ([a-z0-9_]+)\.([a-z0-9_]+)/) {
+                my @fk = ($1, $2, $3);
+                my $cascade = ($line =~ m/CASCADE/) ? 1 : 0;
+                push @fks, [@fk, $cascade];
+            }
+        }
+        if (@fks) {
+            $foreign_keys{$name} = \@fks;
+        }
+        my @pks;
+        foreach my $line (@lines) {
+            if ($line =~ m/([a-z0-9_]+).*?\s*--.*?PK/ || $line =~ m/([a-z0-9_]+).*?SERIAL/) {
+                push @pks, $1;
+            }
+        }
+        if (@pks) {
+            $primary_keys{$name} = \@pks;
+        }
+        push @tables, $name;
+    }
+    @tables = sort(@tables);
+
+    open OUT, ">$FindBin::Bin/../admin/sql$dir/DropTables.sql";
+    print OUT "-- Automatically generated, do not edit.\n";
+    print OUT "\\unset ON_ERROR_STOP\n\n";
+    foreach my $table (@tables) {
+        print OUT "DROP TABLE $table;\n";
+    }
+    close OUT;
+
+    open OUT, ">$FindBin::Bin/../admin/sql$dir/CreateFKConstraints.sql";
+    print OUT "-- Automatically generated, do not edit.\n";
+    print OUT "\\set ON_ERROR_STOP 1\n\n";
+    foreach my $table (@tables) {
+        next unless exists $foreign_keys{$table};
+        my @fks = @{$foreign_keys{$table}};
+        foreach my $fk (@fks) {
+            my $col = $fk->[0];
+            my $ref_table = $fk->[1];
+            my $ref_col = $fk->[2];
+            print OUT "ALTER TABLE $table\n";
+            print OUT "   ADD CONSTRAINT ${table}_fk_${col}\n";
+            print OUT "   FOREIGN KEY ($col)\n";
+            print OUT "   REFERENCES $ref_table($ref_col)";
+            if ($fk->[3]) {
+                print OUT "\n   ON DELETE CASCADE;\n\n";
+            }
+            else {
+                print OUT ";\n\n";
+            }
         }
     }
-    if (@fks) {
-        $foreign_keys{$name} = \@fks;
-    }
-    my @pks;
-    foreach my $line (@lines) {
-        if ($line =~ m/([a-z0-9_]+).*?\s*--.*?PK/ || $line =~ m/([a-z0-9_]+).*?SERIAL/) {
-            push @pks, $1;
+    close OUT;
+
+    open OUT, ">$FindBin::Bin/../admin/sql$dir/DropFKConstraints.sql";
+    print OUT "-- Automatically generated, do not edit.\n";
+    print OUT "\\unset ON_ERROR_STOP\n\n";
+    foreach my $table (@tables) {
+        next unless exists $foreign_keys{$table};
+        my @fks = @{$foreign_keys{$table}};
+        foreach my $fk (@fks) {
+            my $col = $fk->[0];
+            print OUT "ALTER TABLE $table DROP CONSTRAINT ${table}_fk_${col};\n";
         }
     }
-    if (@pks) {
-        $primary_keys{$name} = \@pks;
+    close OUT;
+
+    open OUT, ">$FindBin::Bin/../admin/sql$dir/CreatePrimaryKeys.sql";
+    print OUT "-- Automatically generated, do not edit.\n";
+    print OUT "\\set ON_ERROR_STOP 1\n\n";
+    foreach my $table (@tables) {
+        next unless exists $primary_keys{$table};
+        my @pks = @{$primary_keys{$table}};
+        my $cols = join ", ", @pks;
+        print OUT "ALTER TABLE $table ADD CONSTRAINT ${table}_pkey ";
+        print OUT "PRIMARY KEY ($cols);\n";
     }
-    push @tables, $name;
-}
-@tables = sort(@tables);
+    close OUT;
 
-open OUT, ">$FindBin::Bin/../admin/sql/DropTables.sql";
-print OUT "-- Automatically generated, do not edit.\n";
-print OUT "\\unset ON_ERROR_STOP\n\n";
-foreach my $table (@tables) {
-    print OUT "DROP TABLE $table;\n";
-}
-close OUT;
-
-open OUT, ">$FindBin::Bin/../admin/sql/CreateFKConstraints.sql";
-print OUT "-- Automatically generated, do not edit.\n";
-print OUT "\\set ON_ERROR_STOP 1\n\n";
-foreach my $table (@tables) {
-    next unless exists $foreign_keys{$table};
-    my @fks = @{$foreign_keys{$table}};
-    foreach my $fk (@fks) {
-        my $col = $fk->[0];
-        my $ref_table = $fk->[1];
-        my $ref_col = $fk->[2];
-        print OUT "ALTER TABLE $table\n";
-        print OUT "   ADD CONSTRAINT ${table}_fk_${col}\n";
-        print OUT "   FOREIGN KEY ($col)\n";
-        print OUT "   REFERENCES $ref_table($ref_col)";
-        if ($fk->[3]) {
-            print OUT "\n   ON DELETE CASCADE;\n\n";
-        }
-        else {
-            print OUT ";\n\n";
-        }
+    open OUT, ">$FindBin::Bin/../admin/sql$dir/DropPrimaryKeys.sql";
+    print OUT "-- Automatically generated, do not edit.\n";
+    print OUT "\\unset ON_ERROR_STOP\n\n";
+    foreach my $table (@tables) {
+        next unless exists $primary_keys{$table};
+        print OUT "ALTER TABLE $table DROP CONSTRAINT ${table}_pkey;\n";
     }
+    close OUT;
 }
-close OUT;
 
-open OUT, ">$FindBin::Bin/../admin/sql/DropFKConstraints.sql";
-print OUT "-- Automatically generated, do not edit.\n";
-print OUT "\\unset ON_ERROR_STOP\n\n";
-foreach my $table (@tables) {
-    next unless exists $foreign_keys{$table};
-    my @fks = @{$foreign_keys{$table}};
-    foreach my $fk (@fks) {
-        my $col = $fk->[0];
-        print OUT "ALTER TABLE $table DROP CONSTRAINT ${table}_fk_${col};\n";
+process_tables("");
+process_tables("/vertical/rawdata");
+
+sub process_indexes
+{
+    my ($infile, $outfile) = @_;
+
+    open FILE, "<$FindBin::Bin/../admin/sql/$infile";
+    my $create_indexes_sql = do { local $/; <FILE> };
+    close FILE;
+
+    my @indexes;
+    while ($create_indexes_sql =~ m/CREATE .*?INDEX\s+([a-z0-9_]+)\s+/g) {
+        my $name = $1;
+        push @indexes, $name;
     }
+    @indexes = sort(@indexes);
+
+    open OUT, ">$FindBin::Bin/../admin/sql/$outfile";
+    print OUT "-- Automatically generated, do not edit.\n";
+    print OUT "\\unset ON_ERROR_STOP\n\n";
+    foreach my $index (@indexes) {
+        print OUT "DROP INDEX $index;\n";
+    }
+    close OUT;
 }
-close OUT;
 
-open OUT, ">$FindBin::Bin/../admin/sql/CreatePrimaryKeys.sql";
-print OUT "-- Automatically generated, do not edit.\n";
-print OUT "\\set ON_ERROR_STOP 1\n\n";
-foreach my $table (@tables) {
-    next unless exists $primary_keys{$table};
-    my @pks = @{$primary_keys{$table}};
-    my $cols = join ", ", @pks;
-    print OUT "ALTER TABLE $table ADD CONSTRAINT ${table}_pkey ";
-    print OUT "PRIMARY KEY ($cols);\n";
-}
-close OUT;
-
-open OUT, ">$FindBin::Bin/../admin/sql/DropPrimaryKeys.sql";
-print OUT "-- Automatically generated, do not edit.\n";
-print OUT "\\unset ON_ERROR_STOP\n\n";
-foreach my $table (@tables) {
-    next unless exists $primary_keys{$table};
-    print OUT "ALTER TABLE $table DROP CONSTRAINT ${table}_pkey;\n";
-}
-close OUT;
-
-open FILE, "<$FindBin::Bin/../admin/sql/CreateIndexes.sql";
-my $create_indexes_sql = do { local $/; <FILE> };
-close FILE;
-
-my @indexes;
-while ($create_indexes_sql =~ m/CREATE .*?INDEX\s+([a-z0-9_]+)\s+/g) {
-    my $name = $1;
-    push @indexes, $name;
-}
-@indexes = sort(@indexes);
-
-open OUT, ">$FindBin::Bin/../admin/sql/DropIndexes.sql";
-print OUT "-- Automatically generated, do not edit.\n";
-print OUT "\\unset ON_ERROR_STOP\n\n";
-foreach my $index (@indexes) {
-    print OUT "DROP INDEX $index;\n";
-}
-close OUT;
-
-open FILE, "<$FindBin::Bin/../admin/sql/CreateSearchIndexes.sql";
-my $create_search_indexes_sql = do { local $/; <FILE> };
-close FILE;
-
-my @search_indexes;
-while ($create_search_indexes_sql =~ m/CREATE .*?INDEX\s+([a-z0-9_]+)\s+/g) {
-    my $name = $1;
-    push @search_indexes, $name;
-}
-@search_indexes = sort(@search_indexes);
-
-open OUT, ">$FindBin::Bin/../admin/sql/DropSearchIndexes.sql";
-print OUT "-- Automatically generated, do not edit.\n";
-print OUT "\\unset ON_ERROR_STOP\n\n";
-foreach my $index (@search_indexes) {
-    print OUT "DROP INDEX $index;\n";
-}
-close OUT;
+process_indexes("CreateIndexes.sql", "DropIndexes.sql");
+process_indexes("CreateSearchIndexes.sql", "DropSearchIndexes.sql");
+process_indexes("vertical/rawdata/CreateIndexes.sql", "vertical/rawdata/DropIndexes.sql");
 
 open FILE, "<$FindBin::Bin/../admin/sql/CreateFunctions.sql";
 my $create_functions_sql = do { local $/; <FILE> };

admin/sql/updates/ngs.sql
@@ -82,12 +82,6 @@ INSERT INTO tag_relation SELECT * FROM public.tag_relation;
 
 INSERT INTO artist_tag SELECT * FROM public.artist_tag;
 INSERT INTO label_tag SELECT * FROM public.label_tag;
--- FIXME needs to be done from a perl script using the RAWDATA database
---INSERT INTO release_tag
---    SELECT release_group, tag, SUM(count)
---    FROM public.release_tag
---        JOIN public.album ON release_tag.release = album.id
---    GROUP BY release_group, tag;
 INSERT INTO recording_tag SELECT * FROM public.track_tag;
 
 ------------------------
@@ -120,7 +114,6 @@ INSERT INTO release_group (id, gid, name, type, artist_credit)
         END, artist
     FROM public.release_group a JOIN release_name n ON a.name = n.name;
 
--- FIXME update ratings from a perl script using the RAWDATA database
 INSERT INTO release_group_meta
     (id, lastupdate, releasecount, firstreleasedate_year,
      firstreleasedate_month, firstreleasedate_day)

admin/sql/vertical/rawdata/CreateFKConstraints.sql
@@ -1,21 +1,23 @@
+-- Automatically generated, do not edit.
 \set ON_ERROR_STOP 1
 
--- Alphabetical order by table, then constraint
-
--- No BEGIN/COMMIT here.  Each FK is created in its own transaction;
--- this is mainly because if you're setting up a big database, it
--- could get really annoying if it takes a long time to create the FKs,
--- only for the last one to fail and the whole lot gets rolled back.
--- It should also be more efficient, of course.
-
 ALTER TABLE cdtoc_raw
-    ADD CONSTRAINT cdtoc_raw_fk_release_raw
-    FOREIGN KEY (release)
-    REFERENCES release_raw(id);
+   ADD CONSTRAINT cdtoc_raw_fk_release
+   FOREIGN KEY (release)
+   REFERENCES release_raw(id);
+
+ALTER TABLE edit_note
+   ADD CONSTRAINT edit_note_fk_edit
+   FOREIGN KEY (edit)
+   REFERENCES edit(id);
 
 ALTER TABLE track_raw
-    ADD CONSTRAINT track_raw_fk_release_raw
-    FOREIGN KEY (release)
-    REFERENCES release_raw(id);
+   ADD CONSTRAINT track_raw_fk_release
+   FOREIGN KEY (release)
+   REFERENCES release_raw(id);
 
--- vi: set ts=4 sw=4 et :
+ALTER TABLE vote
+   ADD CONSTRAINT vote_fk_edit
+   FOREIGN KEY (edit)
+   REFERENCES edit(id);
+

admin/sql/vertical/rawdata/CreateIndexes.sql
@@ -8,54 +8,49 @@
 
 -- Alphabetical order by table
 
+CREATE INDEX edit_idx_editor ON edit (editor);
+CREATE INDEX edit_idx_status ON edit (status);
+CREATE INDEX edit_idx_type ON edit (type);
+
+CREATE INDEX edit_note_idx_edit ON edit_note (edit);
+CREATE INDEX vote_idx_edit ON vote (edit);
+
 CREATE INDEX artist_rating_raw_idx_artist ON artist_rating_raw (artist);
 CREATE INDEX artist_rating_raw_idx_editor ON artist_rating_raw (editor);
-    	
+
 CREATE INDEX artist_tag_raw_idx_artist ON artist_tag_raw (artist);
 CREATE INDEX artist_tag_raw_idx_tag ON artist_tag_raw (tag);
-CREATE INDEX artist_tag_raw_idx_moderator ON artist_tag_raw (moderator);
+CREATE INDEX artist_tag_raw_idx_editor ON artist_tag_raw (editor);
 
-CREATE INDEX release_rating_raw_idx_release ON release_rating_raw (release);
-CREATE INDEX release_rating_raw_idx_editor ON release_rating_raw (editor);
-    	
+CREATE INDEX release_group_rating_raw_idx_release_group ON release_group_rating_raw (release_group);
+CREATE INDEX release_group_rating_raw_idx_editor ON release_group_rating_raw (editor);
+
 CREATE INDEX cdtoc_raw_discid ON cdtoc_raw (discid);
 CREATE INDEX cdtoc_raw_trackoffset ON cdtoc_raw (trackoffset);
 CREATE UNIQUE INDEX cdtoc_raw_toc ON cdtoc_raw (trackcount, leadoutoffset, trackoffset);
 
 CREATE INDEX label_tag_raw_idx_label ON label_tag_raw (label);
 CREATE INDEX label_tag_raw_idx_tag ON label_tag_raw (tag);
-CREATE INDEX label_tag_raw_idx_moderator ON label_tag_raw (moderator);
+CREATE INDEX label_tag_raw_idx_editor ON label_tag_raw (editor);
 
 CREATE INDEX release_raw_idx_lastmodified ON release_raw (lastmodified);
 CREATE INDEX release_raw_idx_lookupcount ON release_raw (lookupcount);
 CREATE INDEX release_raw_idx_modifycount ON release_raw (modifycount);
 
-CREATE INDEX release_tag_raw_idx_release ON release_tag_raw (release);
-CREATE INDEX release_tag_raw_idx_tag ON release_tag_raw (tag);
-CREATE INDEX release_tag_raw_idx_moderator ON release_tag_raw (moderator);
+CREATE INDEX release_group_tag_raw_idx_release ON release_group_tag_raw (release_group);
+CREATE INDEX release_group_tag_raw_idx_tag ON release_group_tag_raw (tag);
+CREATE INDEX release_group_tag_raw_idx_editor ON release_group_tag_raw (editor);
 
-CREATE INDEX track_rating_raw_idx_track ON track_rating_raw (track);
-CREATE INDEX track_rating_raw_idx_editor ON track_rating_raw (editor);
-    	
+CREATE INDEX recording_rating_raw_idx_track ON recording_rating_raw (recording);
+CREATE INDEX recording_rating_raw_idx_editor ON recording_rating_raw (editor);
+
 CREATE INDEX track_raw_idx_release ON track_raw (release);
 
-CREATE INDEX track_tag_raw_idx_track ON track_tag_raw (track);
-CREATE INDEX track_tag_raw_idx_tag ON track_tag_raw (tag);
-CREATE INDEX track_tag_raw_idx_moderator ON track_tag_raw (moderator);
+CREATE INDEX recording_tag_raw_idx_track ON recording_tag_raw (recording);
+CREATE INDEX recording_tag_raw_idx_tag ON recording_tag_raw (tag);
+CREATE INDEX recording_tag_raw_idx_editor ON recording_tag_raw (editor);
 
 CREATE INDEX label_rating_raw_idx_label ON label_rating_raw (label);
 CREATE INDEX label_rating_raw_idx_editor ON label_rating_raw (editor);
 
--- unique indexes so that duplicates are not allowed
-CREATE UNIQUE INDEX collection_has_release_join_combined_index ON collection_has_release_join (collection_info, album);
-CREATE UNIQUE INDEX collection_discography_artist_join_combined_index ON collection_discography_artist_join (collection_info, artist);
-CREATE UNIQUE INDEX collection_ignore_release_combined_index ON collection_ignore_release_join (collection_info, album);
-CREATE UNIQUE INDEX collection_watch_artist_combined_index ON collection_watch_artist_join (collection_info, artist);
-
-CREATE INDEX collection_has_release_join_album ON collection_has_release_join (album);
-CREATE INDEX collection_ignore_release_join_album ON collection_ignore_release_join (album);
-CREATE INDEX collection_discography_artist_join_artist ON collection_discography_artist_join (artist);
-CREATE INDEX collection_watch_artist_join_artist ON collection_watch_artist_join (artist);
-
-CREATE UNIQUE INDEX collection_info_moderator ON collection_info (moderator);
 -- vi: set ts=4 sw=4 et :

admin/sql/vertical/rawdata/CreatePrimaryKeys.sql
@@ -1,27 +1,25 @@
+-- Automatically generated, do not edit.
 \set ON_ERROR_STOP 1
 
--- Alphabetical order by table
-
 ALTER TABLE artist_rating_raw ADD CONSTRAINT artist_rating_raw_pkey PRIMARY KEY (artist, editor);
-ALTER TABLE artist_tag_raw ADD CONSTRAINT artist_tag_raw_pkey PRIMARY KEY (artist, tag, moderator);
-
+ALTER TABLE artist_tag_raw ADD CONSTRAINT artist_tag_raw_pkey PRIMARY KEY (artist, editor, tag);
 ALTER TABLE cdtoc_raw ADD CONSTRAINT cdtoc_raw_pkey PRIMARY KEY (id);
-ALTER TABLE collection_info ADD CONSTRAINT collection_info_pkey PRIMARY KEY (id);
-ALTER TABLE collection_ignore_time_range ADD CONSTRAINT collection_ignore_time_range_pkey PRIMARY KEY (id);
-ALTER TABLE collection_watch_artist_join ADD CONSTRAINT collection_watch_artist_join_pkey PRIMARY KEY (id);
-ALTER TABLE collection_discography_artist_join ADD CONSTRAINT collection_discography_artist_join_pkey PRIMARY KEY (id);
-ALTER TABLE collection_ignore_release_join ADD CONSTRAINT collection_ignore_release_join_pkey PRIMARY KEY (id);
-ALTER TABLE collection_has_release_join ADD CONSTRAINT collection_has_release_join_pkey PRIMARY KEY (id);
-
+ALTER TABLE edit ADD CONSTRAINT edit_pkey PRIMARY KEY (id);
+ALTER TABLE edit_artist ADD CONSTRAINT edit_artist_pkey PRIMARY KEY (edit, artist);
+ALTER TABLE edit_label ADD CONSTRAINT edit_label_pkey PRIMARY KEY (edit, label);
+ALTER TABLE edit_note ADD CONSTRAINT edit_note_pkey PRIMARY KEY (id);
+ALTER TABLE edit_recording ADD CONSTRAINT edit_recording_pkey PRIMARY KEY (edit, recording);
+ALTER TABLE edit_release ADD CONSTRAINT edit_release_pkey PRIMARY KEY (edit, release);
+ALTER TABLE edit_release_group ADD CONSTRAINT edit_release_group_pkey PRIMARY KEY (edit, release_group);
+ALTER TABLE edit_work ADD CONSTRAINT edit_work_pkey PRIMARY KEY (edit, work);
 ALTER TABLE label_rating_raw ADD CONSTRAINT label_rating_raw_pkey PRIMARY KEY (label, editor);
-ALTER TABLE label_tag_raw ADD CONSTRAINT label_tag_raw_pkey PRIMARY KEY (label, tag, moderator);
-
+ALTER TABLE label_tag_raw ADD CONSTRAINT label_tag_raw_pkey PRIMARY KEY (label, editor, tag);
+ALTER TABLE recording_rating_raw ADD CONSTRAINT recording_rating_raw_pkey PRIMARY KEY (recording, editor);
+ALTER TABLE recording_tag_raw ADD CONSTRAINT recording_tag_raw_pkey PRIMARY KEY (recording, editor, tag);
+ALTER TABLE release_group_rating_raw ADD CONSTRAINT release_group_rating_raw_pkey PRIMARY KEY (release_group, editor);
+ALTER TABLE release_group_tag_raw ADD CONSTRAINT release_group_tag_raw_pkey PRIMARY KEY (release_group, editor, tag);
 ALTER TABLE release_raw ADD CONSTRAINT release_raw_pkey PRIMARY KEY (id);
-ALTER TABLE release_rating_raw ADD CONSTRAINT release_rating_raw_pkey PRIMARY KEY (release, editor);
-ALTER TABLE release_tag_raw ADD CONSTRAINT release_tag_raw_pkey PRIMARY KEY (release, tag, moderator);
-
 ALTER TABLE track_raw ADD CONSTRAINT track_raw_pkey PRIMARY KEY (id);
-ALTER TABLE track_rating_raw ADD CONSTRAINT track_rating_raw_pkey PRIMARY KEY (track, editor);
-ALTER TABLE track_tag_raw ADD CONSTRAINT track_tag_raw_pkey PRIMARY KEY (track, tag, moderator);
-
--- vi: set ts=4 sw=4 et :
+ALTER TABLE vote ADD CONSTRAINT vote_pkey PRIMARY KEY (id);
+ALTER TABLE work_rating_raw ADD CONSTRAINT work_rating_raw_pkey PRIMARY KEY (work, editor);
+ALTER TABLE work_tag_raw ADD CONSTRAINT work_tag_raw_pkey PRIMARY KEY (work, editor, tag);

commit 01768afa00bac16faaeb5d126a9538a285520bf4

Fix problems introduced in r11671
git-svn-id: http://svn.musicbrainz.org/mb_server/trunk@11677 b0b80210-5d09-0410-99dd-b4bd03f891c0

script/create_test_db.sh
@@ -19,9 +19,9 @@ echo 'CREATE SCHEMA musicbrainz_test;' | ./admin/psql --profile=test RAWDATA
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateIndexes.sql
 ./admin/psql --profile=test READWRITE <./admin/sql/CreateSearchIndexes.sql
 
-./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreateTables.sql
-./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreatePrimaryKeys.sql
-./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreateFKConstraints.sql
-./admin/psql RAWDATA <./admin/sql/vertical/rawdata/CreateIndexes.sql
+./admin/psql --profile=test RAWDATA <./admin/sql/vertical/rawdata/CreateTables.sql
+./admin/psql --profile=test RAWDATA <./admin/sql/vertical/rawdata/CreatePrimaryKeys.sql
+./admin/psql --profile=test RAWDATA <./admin/sql/vertical/rawdata/CreateFKConstraints.sql
+./admin/psql --profile=test RAWDATA <./admin/sql/vertical/rawdata/CreateIndexes.sql
 
 # eof