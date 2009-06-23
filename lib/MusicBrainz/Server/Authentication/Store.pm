1cb2021a7c3d8dbe3ecba937784a649afe4d7adf
Oliver Charles <oliver.g.charles@googlemail.com>
2009-06-23 19:03:01 +0100

    Support logging in and out through the website

--- lib/MusicBrainz/Server/Authentication/Store.pm
+++ lib/MusicBrainz/Server/Authentication/Store.pm
@@ -3,7 +3,7 @@ package MusicBrainz::Server::Authentication::Store;
 use strict;
 use warnings;
 
-use MusicBrainz::Server::Editor;
+use MusicBrainz::Server::Entity::Editor;
 use UserPreference;
 
 sub new
@@ -15,7 +15,7 @@ sub new
 sub find_user
 {
     my ($self, $authinfo, $c) = @_;
-    return $c->model('User')->load({ username => $authinfo->{username} });
+    return $c->model('Editor')->get_by_name($authinfo->{username});
 }
 
 sub for_session
@@ -24,27 +24,18 @@ sub for_session
 
     return {
         'id' => $user->id,
-        'privs' => $user->privs,
         'name' => $user->name,
-        'edits_accepted' => $user->mods_accepted,
-        'has_confirmed_email' => $user->email ? 1 : 0,
-        'prefs' => $user->preferences->{prefs},
     };
 }
 
 sub from_session
 {
-    my ($self, $c, $frozen_user) = @_;
-    my $user = MusicBrainz::Server::Editor->new($c->mb->dbh);
-    $user->id($frozen_user->{id});
-    $user->name($frozen_user->{name});
-    $user->privs($frozen_user->{privs});
-    $user->mods_accepted($frozen_user->{edits_accepted});
-    $user->{has_confirmed_email} = $frozen_user->{has_confirmed_email};
-    
-    my $prefs = UserPreference->new;
-    $prefs->{prefs} = $frozen_user->{prefs} || {};
-    $user->preferences($prefs);
+    my ($self, $c, $frozen) = @_;
+
+    my $user = MusicBrainz::Server::Entity::Editor->new(
+        id => $frozen->{id},
+        name => $frozen->{name}
+    );
 
     return $user;
 }
