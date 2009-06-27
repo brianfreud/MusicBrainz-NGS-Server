5930b1ff79e73f5efef4fedc9cc06f44206d8b76
Oliver Charles <oliver.g.charles@googlemail.com>
2009-06-27 18:22:59 +0100

    Updating with recent changes to edit system

--- lib/MusicBrainz/Server/Edit/Label/Edit.pm
+++ lib/MusicBrainz/Server/Edit/Label/Edit.pm
@@ -12,6 +12,8 @@ extends 'MusicBrainz::Server::Edit';
 
 sub edit_type { $EDIT_LABEL_EDIT }
 sub edit_name { "Edit Label" }
+sub entity_id { shift->label_id }
+sub entity_model { 'Label' }
 
 sub label_id { shift->data->{label} }
 
@@ -76,22 +78,17 @@ sub _mapping
     );
 }
 
-sub create
+sub initialize
 {
-    my ($class, $label, $edit, @args) = @_;
+    my ($self, %args) = @_;
+    my $label = delete $args{label};
     die "You must specify the label object to edit" unless defined $label;
 
-    my %mapping = $class->_mapping;
-    my %old = map {
-        my $mapped = exists $mapping{$_} ? $mapping{$_} : $_;
-        $_ => ref $mapped eq 'CODE' ? $mapped->($label) : $label->$mapped;
-    } keys %$edit;
-
-    return $class->new(data => {
-            old => \%old,
-            new => $edit,
-            label => $label->id
-        }, @args);
+    $self->data({
+        old => $self->_change_hash($label, keys %args),
+        new => \%args,
+        label => $label->id
+    });
 };
 
 override 'accept' => sub
