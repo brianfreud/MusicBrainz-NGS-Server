a347c9acb0a1ccf91040a59c96145a2f5ebfb40b
Oliver Charles <oliver.g.charles@googlemail.com>
2009-07-05 19:22:05 +0100

    Edit artist credits JavaScript component

---
+++ lib/MusicBrainz/Server/Controller/Ajax.pm
@@ -0,0 +1,42 @@
+package MusicBrainz::Server::Controller::Ajax;
+BEGIN { use Moose; extends 'Catalyst::Controller' };
+
+sub search : Local
+{
+    my ($self, $c) = @_;
+
+    my $query = $c->req->query_params->{query};
+    my $type = $c->req->query_params->{type};
+
+    my $json = {};
+    if ($query && $type)
+    {
+        my ($search_results, $hits) = $c->model('DirectSearch')->search($type, $query, 15, 0);
+
+        $json = {
+            results => [ map {
+                my $r = {
+                    name => $_->entity->name,
+                    id => $_->entity->id,
+                    gid => $_->entity->gid
+                };
+                $r->{comment} = $_->entity->comment if ($_->entity->can('comment') && $_->entity->comment);
+                $r->{sort_name} = $_->entity->sort_name if ($_->entity->can('sort_name') && $_->entity->sort_name);
+                $r;
+            } @$search_results ],
+            hits => $hits
+        };
+    }
+    else
+    {
+        $json = {
+            results => [],
+            hits => 0,
+        }
+    }
+
+    $c->stash( json => $json );
+    $c->detach('View::JSON');
+}
+
+1;
