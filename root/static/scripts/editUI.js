6543141074e818057da42ed2a26fd6db408cbb7e
Brian Schweitzer <brian@Silverberg.(none)>
2009-08-04 21:08:43 -0400

    swap in pegboard

--- root/static/scripts/editUI.js
+++ root/static/scripts/editUI.js
@@ -833,11 +833,29 @@ $(function () {
         }
     });
 
-$("#accordion").accordion({ collapsible: true,
-active: 0,
-icons: { 'header': 'ui-icon-plus', 'headerSelected': 'ui-icon-minus' }
-});
-$("#accordion h3 a").css("font-weight","bold");
+/* Code for "multiple sections active at the same time" accordion, using jQuery UI standard css, by Richard D. Worth */
+$("#accordion").addClass("ui-accordion ui-widget ui-helper-reset")
+               .find("h3")
+               .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
+               .prepend('<span class="ui-icon ui-icon-triangle-1-e"/>')
+               .click(function () {
+                   $(this).toggleClass("ui-accordion-header-active")
+                          .toggleClass("ui-state-active")
+                          .toggleClass("ui-state-default")
+                          .toggleClass("ui-corner-bottom")
+                          .find("> .ui-icon")
+                          .toggleClass("ui-icon-triangle-1-e")
+                          .toggleClass("ui-icon-triangle-1-s")
+                          .end()
+                          .next()
+                          .toggleClass("ui-accordion-content-active")
+                          .toggle();
+                   return false;
+               })
+               .next()
+               .addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
+               .hide();
+
 
 
 /* TODO: pre-populate:
